43106ed55f6ef54b57503b57005136d191e96619
thenatural86 <philip.smalls86@gmail.com>
2020-03-30 14:43:16 -0400

    default context value

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,8 +1,13 @@
 import React from "react"
+import LanguageContext from "../contexts/LanguageContext"
 
 class Button extends React.Component {
+  static contextType = LanguageContext
+
   render() {
-    return <button className="ui button primary">Submit</button>
+    // console.log(this.context)
+    const text = this.context === "english" ? "Submit " : "Voorleggen"
+    return <button className="ui button primary">{text}</button>
   }
 }
 
