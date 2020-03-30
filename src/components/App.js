56087a0b8bae1ace978346a0c74f357bc3869721
thenatural86 <philip.smalls86@gmail.com>
2020-03-30 12:43:50 -0400

    form

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,5 @@
 import React from "react"
+import UserCreate from "./UserCreate"
 
 class App extends React.Component {
   state = { language: "english" }
@@ -22,7 +23,7 @@ class App extends React.Component {
             onClick={() => this.onLanguageChange("dutch")}
           />
         </div>
-        {this.state.language}
+        <UserCreate />
       </div>
     )
   }
