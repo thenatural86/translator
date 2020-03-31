9020734413815e4ceb9545b5c9ae621d61093c83
thenatural86 <philip.smalls86@gmail.com>
2020-03-31 18:30:26 -0400

    accessing data with consumer

--- src/components/App.js
+++ src/components/App.js
@@ -10,7 +10,6 @@ class App extends React.Component {
       <div className="ui container">
         <LanguageStore>
           <LanguageSelector />
-
           {/* value prop is special for Provider. The value is what we want to take from App component and put into context object */}
           <ColorContext.Provider value="red">
             <UserCreate />
--- src/components/Button.js
+++ src/components/Button.js
@@ -3,7 +3,9 @@ import React from "react"
 import LanguageContext from "../contexts/LanguageContext"
 import ColorContext from "../contexts/ColorContext"
 
+// Do not need a context type when using a Consumer
 class Button extends React.Component {
+  // helper method that compares the value of the language property
   renderSubmit = language => {
     return language === "english" ? "Submit" : "Voorleggen"
   }
@@ -11,7 +13,11 @@ class Button extends React.Component {
   renderButton = color => {
     return (
       <button className={`ui button ${color}`}>
+        {/* Call the Consumer component on the language context object  */}
         <LanguageContext.Consumer>
+          {/* provide one child passed into consumer, which is q function. */}
+          {/* gets called with the current value inside of context object */}
+          {/* invokes the renderSubmit callback function */}
           {({ language }) => this.renderSubmit(language)}
         </LanguageContext.Consumer>
       </button>
